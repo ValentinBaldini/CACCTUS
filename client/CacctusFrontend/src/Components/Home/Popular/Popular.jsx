385d3fc8fd7d227ee52bee46a4867c3a091d38f1
alexBeltramella <beatsinka@gmail.com>
2025-06-05 12:52:14 -0300

    05/06 creacion de archivos popular css y jsx

---
+++ client/CacctusFrontend/src/Components/Home/Popular/Popular.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./popular.css"
+
+export const Popular = () => {
+    return (
+        <div className="popular-container">
+            <div className="popular">
+                
+            </div>
+        </div>
+    )
+}
